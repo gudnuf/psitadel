7ff1e9c957c139916385b2266c7836adcfee42ca
daGoodenough <dgoodenough@proton.me>
2023-04-04 14:40:48 -0700

    Import bootstrap css

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
-import { Route, Routes } from 'react-router-dom'
 
-import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </div>
   );

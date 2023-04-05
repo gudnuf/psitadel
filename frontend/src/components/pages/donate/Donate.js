bc03c36921d926bac5ddf09578651308f4e61709
daGoodenough <dgoodenough@proton.me>
2023-04-04 17:08:10 -0700

    Add other pages we will link to

---
+++ frontend/src/components/pages/donate/Donate.js
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap';
+
+const Donate = () => (
+  <Container>
+    This page will have ways to donate. QR codes with lightning and on chain
+    addresses Direct to contact page for more info
+  </Container>
+);
+
+export default Donate;

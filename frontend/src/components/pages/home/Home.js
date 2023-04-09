fe63f05a288dc303028201eb3f5edf3af8b0777d
daGoodenough <dgoodenough@proton.me>
2023-04-08 19:30:25 -0700

    Set up join and about sections

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -3,6 +3,7 @@ import { Container, Row } from 'react-bootstrap';
 import HomeHeader from './HomeHeader';
 import TopNav from '../../TopNav';
 import Values from './Values';
+import JoinAndAbout from './JoinAndAbout';
 
 const Home = () => {
   const theme = 'light';
@@ -11,7 +12,10 @@ const Home = () => {
       <TopNav theme={theme} />
       <Container className="home d-flex flex-column justify-content-around">
         <HomeHeader theme={theme} />
-        <Values />
+        <Values className="section values-section" />
+        <Row className="align-items-center justify-content-center section values-section">
+          <JoinAndAbout className="" />
+        </Row>
       </Container>
     </>
   );

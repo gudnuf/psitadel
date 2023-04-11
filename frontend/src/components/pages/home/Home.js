3cdf6b1c14ad4bb74e80853e49a75bb49d317820
daGoodenough <dgoodenough@proton.me>
2023-04-11 14:36:59 -0700

    Add a footer

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -1,23 +1,19 @@
 import { Container, Row } from 'react-bootstrap';
 
 import HomeHeader from './HomeHeader';
-import TopNav from '../../TopNav';
 import Values from './Values';
 import JoinAndAbout from './JoinAndAbout';
 
 const Home = () => {
   const theme = 'light';
   return (
-    <>
-      <TopNav theme={theme} />
-      <Container className="home d-flex flex-column justify-content-around">
-        <HomeHeader theme={theme} />
-        <Values className="section values-section" />
-        <Row className="align-items-center justify-content-center section values-section">
-          <JoinAndAbout className="" />
-        </Row>
-      </Container>
-    </>
+    <Container className="home d-flex flex-column justify-content-around">
+      <HomeHeader theme={theme} />
+      <Values className="section values-section" />
+      <Row className="align-items-center justify-content-center section values-section">
+        <JoinAndAbout className="" />
+      </Row>
+    </Container>
   );
 };
 

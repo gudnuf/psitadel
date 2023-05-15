91db8682906ca9762d2ca75a5d4b5e8fe2721183
daGoodenough <dgoodenough@proton.me>
2023-05-15 16:46:17 -0700

    restyle

--- frontend/src/components/pages/about/About.js
+++ frontend/src/components/pages/about/About.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './About.css';
 
 const About = () => (
   <Container>
-    This page will contain information on what the Psitadel is and that jazz
+    <div className="about-page">
+      <h1>About Psitadel</h1>
+      <p>
+        A portmanteau of Psilocybin Citadel, Psitadel is a safe ground for
+        psychedelic exploration utilizing the Bitcoin network.{' '}
+      </p>
+      <h2>Our Mission</h2>
+      <p>
+        Formed online in 2023, Psitadel is an open-source DAO (decentralized
+        autonomous organization) to advocate for mental well-being with
+        psychedelic education facilitating direct access to entheogenic
+        medicines in a self-sovereign and responsible manner.{' '}
+      </p>
+      <p>
+        The importance of cultural acceptance and rewriting of baseless stigma
+        is foundational to these ends.
+      </p>
+      <p>
+        Equally paramount is our belief that utilizing the Bitcoin network,
+        which shares a fundamental structure of decentralization and autonomy,
+        will highlight the parallel necessity for economic and social reform.
+      </p>
+      <p>
+        Our Mission is to facilitate real-world gatherings to promote the use of
+        tools which allow the holistic healing of society’s ills regarding
+        mental, physical, spiritual, social, and financial health.
+      </p>
+      <h2>Funding</h2>
+      <p>
+        We are currently self-funded working through volunteership building the
+        mycelial networks of digital, financial, physical, and human
+        connections.
+      </p>
+      <p>
+        All donated funds will be used to grow these foundational layers of
+        Psitadel.
+      </p>
+    </div>
   </Container>
 );
 
